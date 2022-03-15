02be1140fc62525518ce427d13db0825549b128f
azepelayo <Aze.Pelayo@mrm.com>
2022-03-16 00:08:49 +0800

    Create mobile styling for landing page

---
+++ main.js
@@ -0,0 +1,12 @@
+const toggleMenu = (() => {
+
+    const burgerIcon = document.querySelector('.cmp-burgerIconContainer__icon');
+    const burgerIconContainer = document.querySelector('.cmp-burgerIconContainer');
+    const nav = document.querySelector('.navContainer');
+
+    burgerIcon.addEventListener('click', ()=> {
+        nav.classList.toggle('expanded');
+        burgerIconContainer.classList.toggle('expanded');
+    })
+
+})();
